b9f63ec3f206ffe49f847983e24d951950866dac
johnnysuri-dev <johnnysuri-dev>
2023-11-21 11:44:21 -0400

    actualizacion de conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,7 +1,10 @@
 
 <?php
 
-$conexion = mysqli_connect('localhost','root','','sistemasoperativos')
+$conexion = mysqli_connect('localhost','id21551536_root','Nadaquever1234#','id21551536_gestorarchivo')
     or die ('no existe la conexion');
+
+   // $conexion = mysqli_connect('localhost','root','','sistemasoperativos')
+    //or die ('no existe la conexion');
 
  ?>
